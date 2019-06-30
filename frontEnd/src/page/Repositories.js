d175ff976b74f7441c3630b950d4c84f4f0842e2
Suele <suelen.ads.guimaraes@gmail.com>
2019-06-30 03:00:56 -0300

    Add the view of repositories of the searched user.

--- frontEnd/src/page/Repositories.js
+++ frontEnd/src/page/Repositories.js
@@ -1,5 +1,8 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Moment from "react-moment";
+import { Link } from "react-router-dom";
+import "./Repositories.css";
 
 class Repositories extends React.Component {
   state = {
@@ -16,10 +19,39 @@ class Repositories extends React.Component {
   }
 
   render() {
+    const { repos } = this.state;
+    const { match } = this.props;
     return (
       <div>
         <Navbar />
-        Repositories
+        {repos.map(repo => (
+          <div className="card card-repos" key={repo.id}>
+            <Link
+              target="_blank"
+              rel="noopener noreferrer"
+              to={{
+                pathname: `https://github.com/${match.params.user}/${repo.name}`
+              }}
+            >
+              <h5 className="card-header">{repo.name}</h5>
+            </Link>
+            <div className="card-body">
+              <p className="card-text">{repo.description}</p>
+              <p className="card-text">{repo.language}</p>
+              <p className="card-text">
+                <small className="text-muted">
+                  Created Repository:
+                  <Moment format=" DD/MM/YYYY">{repo.created_at}</Moment>
+                </small>
+                <small className="text-muted">
+                  Last Update:
+                  <Moment format=" DD/MM/YYYY">{repo.updated_at}</Moment>
+                </small>
+              </p>
+              <div />
+            </div>
+          </div>
+        ))}
       </div>
     );
   }
