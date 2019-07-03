84d0acca2d63f403d3646576a4744513a4e6cce3
Suele <suelen.ads.guimaraes@gmail.com>
2019-07-03 17:21:46 -0300

    Adjusts the port application.

--- backEnd/index.js
+++ backEnd/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const PORT = process.env.PORT || 3030;
 app.use(cors());
 app.use(require("./src/router"));
 
-app.listen(3030, ()=> {
+app.listen(PORT, ()=> {
   console.log("run server port 3030");
 })
